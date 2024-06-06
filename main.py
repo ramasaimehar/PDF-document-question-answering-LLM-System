859aa7ede4b89ded1e1a9a508405a0bf44062ccd
ramasaimehar <119723769+ramasaimehar@users.noreply.github.com>
2024-06-06 23:32:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pinecone
 from langchain.vectorstores import Pinecone
 
 os.environ["OPENAI_API_KEY"] = openai_key
-directory = 'data'
+directory = 'data'# PUT YOUR DATA DIRECTORY NAME
 
 def load_docs(directory):
     loader = DirectoryLoader(directory)
@@ -36,11 +36,11 @@ query_result = embeddings.embed_query("Hello world")
 print(len(query_result))
 
 pinecone.init(
-    api_key="16ae52a0-55e0-418c-bf15-c9e4424912f4",
-    environment="gcp-starter"
+    api_key="YOUR PINECONE API KEY",
+    environment="YOUR PINECONE ENVIRONMENT NAME"
 )
 
-index_name = "ai"
+index_name = "YOUR PINECONE INDEX NAME"
 
 index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
 
@@ -74,26 +74,26 @@ def get_answer(query):
 
 import streamlit as st
 
-st.title('Fitness Tutor')
-input_text = st.text_input("Search Your Training Program ")
+st.title('PDF Document Question Answering:books:')
+input_text = st.text_input("Ask me a question!")
 
 template = '''
           
 Client: I want to focus on strength training. What exercises should I include in my workout program?
 
-Fitness Tutor: For strength training, I recommend incorporating compound exercises like squats, deadlifts, bench presses, and overhead presses into your workout program. These exercises target multiple muscle groups and are highly effective for building overall strength. Additionally, include exercises such as pull-ups, rows, and lunges to further enhance your muscle development. Remember to start with lighter weights and gradually increase the intensity as you progress. Don't forget to prioritize proper form and technique to avoid injuries.
+Bot: For strength training, I recommend incorporating compound exercises like squats, deadlifts, bench presses, and overhead presses into your workout program. These exercises target multiple muscle groups and are highly effective for building overall strength. Additionally, include exercises such as pull-ups, rows, and lunges to further enhance your muscle development. Remember to start with lighter weights and gradually increase the intensity as you progress. Don't forget to prioritize proper form and technique to avoid injuries.
           {context}
           Client: {human_input}
-          Fitness Tutor:'''
+          Bot:'''
 
 
 
 prompt = PromptTemplate(input_variables=["context", "human_input"], template=template)
 if input_text:
-    documents = load_docs('data')
+    documents = load_docs('--YOUR DATA DIRECTORY NAME--')
     docs = split_docs(documents)
     embeddings = OpenAIEmbeddings()
-    index_name = "fitnessinfo"
+    index_name = "----YOUR--INDEX--NAME-----"
     index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
     llm = OpenAI(model_name='gpt-3.5-turbo')
     
